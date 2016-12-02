59ddd1e8a0c2e973597ddef190146a71fb4ffdbb
boldowa <boldowa.q@gmail.com>
2016-12-02 23:02:49 +0900

    Update seq command number

--- spc/seqcmd.s
+++ spc/seqcmd.s
@@ -18,61 +18,61 @@
 ; Command Jump Table
 ;--------------------------------------------------
 CmdTable:
-	.dw	CmdSetInstrument
-	.dw	CmdVolume
-	.dw	CmdPanpot
 	.dw	CmdJump
-	.dw	CmdTempo
-	.dw	CmdGlobalVolume
-	.dw	CmdEchoOn
-	.dw	CmdEchoOff
-	.dw	CmdEchoParam
-	.dw	CmdSetFIR
-	.dw	CmdPortamentoOn
-	.dw	CmdPortamentoOff
-	.dw	CmdPitchModulation
-	.dw	CmdTremolo
 	.dw	CmdSubroutine
 	.dw	CmdSubroutineReturn
 	.dw	CmdSubroutineBreak
-	.dw	CmdPitchBend
-	.dw	CmdTempoFade
-	.dw	CmdVolumeFade
+	.dw	CmdSetInstrument
+	.dw	CmdGlobalVolume
 	.dw	CmdGlobalVolumeFade
-	.dw	CmdTranspose
-	.dw	CmdRelativeTranspose
+	.dw	CmdVolume
+	.dw	CmdVolumeFade
+	.dw	CmdTremolo
+	.dw	CmdPanpot
 	.dw	CmdPanFade
 	.dw	CmdPanVibration
+	.dw	CmdTempo
+	.dw	CmdTempoFade
+	.dw	CmdTranspose
+	.dw	CmdRelativeTranspose
+	.dw	CmdPitchBend
 	.dw	CmdPitchEnvelope
+	.dw	CmdPitchModulation
+	.dw	CmdEchoParam
+	.dw	CmdSetFIR
+	.dw	CmdEchoOn
+	.dw	CmdEchoOff
+	.dw	CmdPortamentoOn
+	.dw	CmdPortamentoOff
 	.dw	CmdCmdCallArg0
 
 CmdLengthTable:
-	.db	1	; CmdSetInstrument
-	.db	1	; CmdVolume
-	.db	1	; CmdPanpot
 	.db	2	; CmdJump
-	.db	1	; CmdTempo
-	.db	1	; CmdGlobalVolume
-	.db	0	; CmdEchoOn
-	.db	0	; CmdEchoOff
-	.db	3	; CmdEchoParam
-	.db	8	; CmdSetFIR
-	.db	0	; CmdPortamentoOn
-	.db	0	; CmdPortamentoOff
-	.db	3	; CmdPitchModulation
-	.db	3	; CmdTremolo
 	.db	3	; CmdSubroutine
 	.db	0	; CmdSubroutineReturn
 	.db	0	; CmdSubroutineBreak
-	.db	3	; CmdPitchBend
-	.db	2	; CmdTempoFade
-	.db	2	; CmdVolumeFade
+	.db	1	; CmdSetInstrument
+	.db	1	; CmdGlobalVolume
 	.db	2	; CmdGlobalVolumeFade
-	.db	1	; CmdTranspose
-	.db	1	; CmdRelativeTranspose
+	.db	1	; CmdVolume
+	.db	2	; CmdVolumeFade
+	.db	3	; CmdTremolo
+	.db	1	; CmdPanpot
 	.db	2	; CmdPanFade
 	.db	2	; CmdPanVibration
+	.db	1	; CmdTempo
+	.db	2	; CmdTempoFade
+	.db	1	; CmdTranspose
+	.db	1	; CmdRelativeTranspose
+	.db	3	; CmdPitchBend
 	.db	3	; CmdPitchEnvelope
+	.db	3	; CmdPitchModulation
+	.db	3	; CmdEchoParam
+	.db	8	; CmdSetFIR
+	.db	0	; CmdEchoOn
+	.db	0	; CmdEchoOff
+	.db	0	; CmdPortamentoOn
+	.db	0	; CmdPortamentoOff
 	.db	1	; CmdCmdCallArg0
 
 ;--------------------------------------------------
